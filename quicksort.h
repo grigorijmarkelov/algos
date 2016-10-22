a0610fde0ee142bbb44c5470ef1a0e5e16e0b26a
Grigoriy Markelov <markeloff.g@yandex.ru>
2016-10-22 19:27:11 +0500

    ending up with a working version of algorithm

--- quicksort.h
+++ quicksort.h
@@ -4,43 +4,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-# Here we will break process into logical parts:
-# choosing pivot
-# swapping
-# actually sorting
-
-void swap(int *first_int, int *second_int)
+void swap(int *first_int,int *second_int);
+int pivoting(int first_val,int second_val);
+void quicksort(int arr[],int first,int last);
+// Here we will break process into logical parts:
+// choosing pivot
+// swapping
+// actually sorting
+void swap(int *first_int,int *second_int)
 {
     	int temp = *first_int;
     	*first_int = *second_int;
     	*second_int = temp;
 }
 
-int pivoting(int first_int, int second_int)
+int pivoting(int first_val,int second_val)
 {
-        return (first_int + second_int)/2;
+        return ((first_val + second_val)/2);
 }
 
-void quicksort(int arr[], int first, int last)
+void quicksort(int arr[],int first,int last)
 {
-    	int pivot_point, pivot_value, i, j;
-    	pivot_point = pivoting(first, last);
-    	pivot_value = arr[pivot_point];
+    	int pivot_point,pivot_value,i,j;
+    	if(first < last)
+	{
+		pivot_point = pivoting(first,last);
+		swap(&arr[first],&arr[pivot_point]);
+    		pivot_value = arr[first];
     
-    i = first;
-    j = last;
-    while(i <= j)
-    {       
-	    while((i <= n) && (arr[i] <= pivot_value))
-		    i++;
-    	    while((j >= m) && (arr[j] > pivot_value))
-		    j--;
-	    if(i < y)
-		    swap(&arr[i], &arr[j];
-    }
-    swap(&arr[i], &arr[j]);
+    		i = first+1;
+    		j = last;
+    		while(i <= j)
+    		{	       
+	    		while((i <= last) && (arr[i] <= pivot_value))
+		    		i++;
+    	    		while((j >= first) && (arr[j] > pivot_value))
+		    		j--;
+	    		if(i < j)
+		    		swap(&arr[i],&arr[j]);
+    		}
+    		swap(&arr[first],&arr[j]);
 
-    quicksort(arr, m, pivot_point-1);
-    quicksort(arr,pivot_point+1, n);
+    		quicksort(arr,first,j-1);
+    		quicksort(arr,j+1,last);
+	}
 }
 #endif
